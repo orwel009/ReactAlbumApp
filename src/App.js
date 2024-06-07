29ab96d958efb96973709c9b59961a94773ee701
orwel009 <orwelvincent1@gmail.com>
2024-06-07 23:42:30 +0530

    Search Delete commmit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddAlbum from './components/AddAlbum';
+import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<AddAlbum/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/delete' element={<Delete/>}/>
       </Routes>
     </BrowserRouter>
   );
